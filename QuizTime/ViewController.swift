09d3e77f698120d65265df69ad16f691c415748f
aananya <astream.h@outlook.com>
2018-06-26 13:00:13 +0530

    Basic working done.

--- QuizTime/ViewController.swift
+++ QuizTime/ViewController.swift
@@ -10,16 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
     //Static Questions and answers
+    let questions = ["2+7?","1+2?","20+20?"]
+    let answers = [["9","27","55"],["3","12","15"],["40","60","99"]]
     
     //variables
-
+    var currentQues = 0
+    var correctAnsPlace:UInt32 = 0 //tracks where the right answer is
+    
+    
     //MARK: Label
     @IBOutlet weak var lbl: UILabel!
     
     //MARK: Buttons
     @IBAction func action(_ sender: Any)
     {
+        if ((sender as AnyObject).tag == Int(correctAnsPlace)){
+            print("Right!")
+        }
+        else{
+            print("Wrong!!!!!")
+        }
         
+        if (currentQues != questions.count){
+            showNewQuestion()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        showNewQuestion()
+    }
+    
+    func showNewQuestion()
+    {
+        lbl.text = questions[currentQues]
+        correctAnsPlace = arc4random_uniform(3)+1
+        
+        var button:UIButton = UIButton()
+        var x = 1
+        
+        for i in 1...3{
+            button = view.viewWithTag(i) as! UIButton
+            
+            if (i==Int(correctAnsPlace)){
+                button.setTitle(answers[currentQues][0], for: .normal)
+            }
+            else {
+                button.setTitle(answers[currentQues][x], for: .normal)
+                x = 2
+            }
+            
+        }
+        currentQues += 1
     }
     
     override func viewDidLoad() {
