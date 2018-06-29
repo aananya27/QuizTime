f4404d2facc038ced9dbadd89acff9e058fbaa2b
aananya <astream.h@outlook.com>
2018-06-29 18:34:03 +0530

    Ading installation code- firebase

--- QuizTime/AppDelegate.swift
+++ QuizTime/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+//ADD THIS line
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //ADDING THIS LINE HERE
+         FirebaseApp.configure()
         return true
     }
 
